de5a98f995083ba020e4288a16b0fea51e613975
Manthan Kuber <manthankuber873@gmail.com>
2022-11-02 14:59:26 +0530

    style(Input): red border on error

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -13,21 +13,27 @@ const Input = ({
     <>
       <div className="relative">
         <label htmlFor={inputName}>
-          <span className="font-semibold tracking-wide text-gray-700 dark:text-white">
+          <span className="font-semibold tracking-wide text-neutral-700 dark:text-white">
             {label}
             <MdEmail className="absolute  bottom-[11px] left-3 h-5 w-5 text-neutral-400" />
           </span>
           <input
             {...register(inputName)}
-            className="mt-1 w-full rounded-lg border border-neutral-500 py-2 px-10 focus:border-transparent
-             "
+            className={`mt-1 w-full rounded-lg border-2 ${
+              isError
+                ? "border-red-500 focus:border-red-500"
+                : "border-neutral-500 focus:border-lime-300"
+            } py-2 px-10 focus:border-transparent focus:outline-none
+            `}
             placeholder={placeholder}
             name={inputName}
             type={type}
           />
         </label>
       </div>
-      {isError && <small className="mt-2 text-red-500"> {errorMessage}</small>}
+      {isError && (
+        <small className="mt-2 font-medium text-red-500"> {errorMessage}</small>
+      )}
     </>
   );
 };
