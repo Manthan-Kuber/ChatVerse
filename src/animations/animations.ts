f227da9280f28fa593cf0e8f47e4be67202b852e
Manthan Kuber <manthankuber873@gmail.com>
2022-11-22 10:54:23 +0530

    Revert "refactor: make animations file typesafe"
    This reverts commit 10367a79bd39fa3e4aa170a1417133b16070aa63.

--- src/animations/animations.ts
+++ src/animations/animations.ts
@@ -1,43 +1,37 @@
-import { Variants } from "framer-motion";
-
 const easing = [0.6, 0.05, -0.01, 0.99];
 
-type AnimationsKey = "fadeInUp" | "fadeInOut" | "stagger";
-
-const animations: Record<AnimationsKey, Variants> = {
-  fadeInUp: {
-    initial: {
-      y: 60,
-      opacity: 0,
-    },
-    animate: {
-      y: 0,
-      opacity: 1,
-      transition: {
-        duration: 0.2,
-        ease: easing,
-      },
-    },
+export const fadeInUp = {
+  initial: {
+    y: 60,
+    opacity: 0,
   },
-  fadeInOut: {
-    initial: { opacity: 0 },
-    animate: {
-      opacity: 1,
-      transition: {
-        duration: 0.2,
-        ease: easing,
-      },
-    },
-    exit: { opacity: 0 },
-  },
-  stagger: {
-    animate: {
-      transition: {
-        staggerChildren: 0.2,
-        when: "afterChildren",
-      },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.2,
+      ease: easing,
     },
   },
 };
 
-export const { fadeInOut, fadeInUp, stagger } = animations;
+export const fadeInOut = {
+  initial: { opacity: 0 },
+  animate: {
+    opacity: 1,
+    transition: {
+      duration: 0.2,
+      ease: easing,
+    },
+  },
+  exit: { opacity: 0 },
+};
+
+export const stagger = {
+  animate: {
+    transition: {
+      staggerChildren: 0.2,
+      when: "afterChildren",
+    },
+  },
+};
