2304d6b74e660e843ed46559626f501ccfeb3f7e
Manthan Kuber <manthankuber873@gmail.com>
2022-11-22 11:14:02 +0530

    feat: make animations file typesafe

--- src/animations/animations.ts
+++ src/animations/animations.ts
@@ -1,37 +1,43 @@
+import { Variants } from "framer-motion";
+
 const easing = [0.6, 0.05, -0.01, 0.99];
 
-export const fadeInUp = {
-  initial: {
-    y: 60,
-    opacity: 0,
+type AnimationsKeys = "fadeInUp" | "fadeInOut" | "stagger";
+
+const animations: Record<AnimationsKeys, Variants> = {
+  fadeInUp: {
+    initial: {
+      y: 60,
+      opacity: 0,
+    },
+    animate: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.2,
+        ease: easing,
+      },
+    },
   },
-  animate: {
-    y: 0,
-    opacity: 1,
-    transition: {
-      duration: 0.2,
-      ease: easing,
+  fadeInOut: {
+    initial: { opacity: 0 },
+    animate: {
+      opacity: 1,
+      transition: {
+        duration: 0.2,
+        ease: easing,
+      },
+    },
+    exit: { opacity: 0 },
+  },
+  stagger: {
+    animate: {
+      transition: {
+        staggerChildren: 0.2,
+        when: "afterChildren",
+      },
     },
   },
 };
 
-export const fadeInOut = {
-  initial: { opacity: 0 },
-  animate: {
-    opacity: 1,
-    transition: {
-      duration: 0.2,
-      ease: easing,
-    },
-  },
-  exit: { opacity: 0 },
-};
-
-export const stagger = {
-  animate: {
-    transition: {
-      staggerChildren: 0.2,
-      when: "afterChildren",
-    },
-  },
-};
+export const { fadeInUp, fadeInOut, stagger } = animations;
