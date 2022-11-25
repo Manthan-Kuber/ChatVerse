446f98fb62583df4ab73b04ffb59e3766b6bfb91
Manthan Kuber <manthankuber873@gmail.com>
2022-11-25 23:31:44 +0530

    refactor: remove code not required

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,14 @@
-import { useSession, signOut } from "next-auth/react";
 import Link from "next/link";
 import ChatVerseText from "./ChatVerseText";
 import ThemeChanger from "./ThemeChanger";
 
 const Header = () => {
-  const { data: session } = useSession();
-
   return (
-    <header className=" flex w-full max-w-7xl mx-auto items-center justify-between p-4">
+    <header className=" mx-auto flex w-full max-w-7xl items-center justify-between p-4">
       <Link href="/">
         <ChatVerseText textSize="text-2xl" mdTextSize="md:text-3xl" />
       </Link>
-      <div className="flex items-center ">
-        {/* If you need to redirect to another page but you want to avoid a page reload, you can try: const data = await signOut({redirect: false, callbackUrl: "/foo"}) where data.url is the validated URL you can redirect the user to without any flicker by using Next.js's useRouter().push(data.url) */}
-        {/* TODO Remove later */}
-        {session && session.user?.name}
-        {/* TODO Move to sidebar */}
-        {session && (
-          <button onClick={() => signOut({ redirect: false })}>Logout</button>
-        )}
-        <div>
-          <ThemeChanger />
-        </div>
-      </div>
+      <ThemeChanger />
     </header>
   );
 };
