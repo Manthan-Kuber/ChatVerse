e45ded1b5f9d71e9d8ee3a594647b43f89b70cc3
Manthan Kuber <manthankuber873@gmail.com>
2022-11-29 19:54:16 +0530

    feat: add chat input ui

--- src/pages/chats.tsx
+++ src/pages/chats.tsx
@@ -4,10 +4,12 @@ import { AnimatePresence } from "framer-motion";
 import Sidebar from "../components/Sidebar";
 import Menu from "../components/Menu";
 import ChatsHeader from "../components/ChatsHeader";
+import { IoMdSend } from "react-icons/io";
 
 const chats = () => {
   const { width: screenWidth } = useWindowSize();
   const [isOpen, setIsOpen] = useState(false);
+  const [hasTyped, setHasTyped] = useState(false);
   const wByN = (n: number) => screenWidth && screenWidth * n;
   // TODO Add page transition animation
   return (
@@ -28,8 +30,19 @@ const chats = () => {
         )}
         <div className="sm:px-2 sm:pt-8">
           <ChatsHeader setIsOpen={setIsOpen} />
-          <main className="min-h-[calc(100vh-4.5rem)] bg-neutral-300 bg-opacity-10 px-4  sm:min-h-[calc(100vh-6.5rem)]">
-            Main section
+          <main className="flex min-h-[calc(100vh-4.5rem)] flex-col  bg-neutral-300 bg-opacity-10 sm:min-h-[calc(100vh-6.5rem)] sm:pb-16">
+            <div className="flex-1 ">Main section</div>
+            <div className="flex items-center gap-4 px-4 pt-2">
+              <input
+                className="container rounded-full bg-neutral-500/10 px-4 py-2 focus:outline-none "
+                placeholder={`Message channel name`}
+              />
+              {!hasTyped && (
+                <button className="rounded-full bg-lime-300 p-2">
+                  <IoMdSend className="h-5 w-5 text-white" />
+                </button>
+              )}
+            </div>
           </main>
         </div>
       </div>
