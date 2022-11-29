f0d442255b168b8c49e4d890c8530a8540fe50e3
Manthan Kuber <manthankuber873@gmail.com>
2022-11-29 20:17:45 +0530

    feat: add submit and change handlers

--- src/pages/chats.tsx
+++ src/pages/chats.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useState } from "react";
+import React, { FormEvent, ReactElement, useState } from "react";
 import useWindowSize from "../hooks/useWindowSize";
 import { AnimatePresence } from "framer-motion";
 import Sidebar from "../components/Sidebar";
@@ -9,8 +9,15 @@ import { IoMdSend } from "react-icons/io";
 const chats = () => {
   const { width: screenWidth } = useWindowSize();
   const [isOpen, setIsOpen] = useState(false);
-  const [hasTyped, setHasTyped] = useState(false);
+  const [message, setMessage] = useState("");
   const wByN = (n: number) => screenWidth && screenWidth * n;
+
+  const handleSubmit = (e:FormEvent) => {
+    e.preventDefault()
+    alert(message) //TODO Emit Socket event here 
+    setMessage("")
+  }
+
   // TODO Add page transition animation
   return (
     <>
@@ -32,16 +39,20 @@ const chats = () => {
           <ChatsHeader setIsOpen={setIsOpen} />
           <main className="flex min-h-[calc(100vh-4.5rem)] flex-col  bg-neutral-300 bg-opacity-10 sm:min-h-[calc(100vh-6.5rem)] sm:pb-16">
             <div className="flex-1 ">Main section</div>
-            <div className="flex items-center gap-4 px-4 pt-2">
-              <input
-                className="container rounded-full bg-neutral-500/10 px-4 py-2 focus:outline-none "
-                placeholder={`Message channel name`}
-              />
-              {!hasTyped && (
-                <button className="rounded-full bg-lime-300 p-2">
-                  <IoMdSend className="h-5 w-5 text-white" />
-                </button>
-              )}
+            <div className="px-4 pt-2">
+              <form className="flex items-center gap-4"onSubmit={handleSubmit} >
+                <input
+                  className="container rounded-full bg-neutral-500/10 px-4 py-2 focus:outline-none "
+                  placeholder={`Message channel name`}
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                />
+                {message.length > 0 && (
+                  <button className="rounded-full bg-lime-400 hover:bg-lime-500 p-2" type="submit" disabled={!(message.length > 0)} >
+                    <IoMdSend className="h-5 w-5 text-white" />
+                  </button>
+                )}
+              </form>
             </div>
           </main>
         </div>
