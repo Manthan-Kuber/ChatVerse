368060811271803d9b4d1cf2f4e9a4aafc207783
Manthan Kuber <manthankuber873@gmail.com>
2022-11-30 17:28:33 +0530

    feat: add socket.io client

--- src/pages/chats.tsx
+++ src/pages/chats.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, ReactElement, useState } from "react";
+import React, { FormEvent, KeyboardEvent, ReactElement, useState } from "react";
 import useWindowSize from "../hooks/useWindowSize";
 import { AnimatePresence, motion } from "framer-motion";
 import Sidebar from "../components/Sidebar";
@@ -6,6 +6,9 @@ import Menu from "../components/Menu";
 import ChatsHeader from "../components/ChatsHeader";
 import { IoMdSend } from "react-icons/io";
 import { appearIntoView } from "../animations/animations";
+import { io } from "socket.io-client";
+
+const socket = io("ws://localhost:6969");
 
 const chats = () => {
   const { width: screenWidth } = useWindowSize();
@@ -13,62 +16,69 @@ const chats = () => {
   const [message, setMessage] = useState("");
   const wByN = (n: number) => screenWidth && screenWidth * n;
 
-  const handleSubmit = (e: FormEvent) => {
+  const handleSubmit = (
+    e: FormEvent<HTMLFormElement> | KeyboardEvent<HTMLInputElement>
+  ) => {
     e.preventDefault();
-    alert(message); //TODO Emit Socket event here
-    setMessage("");
+    console.log(message);
+    try {
+      socket.emit("send_message", message);
+      setMessage("");
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   // TODO Add page transition animation
   return (
-    <>
-      <div className="mx-auto max-w-7xl sm:grid sm:grid-cols-[1fr_2fr]">
-        {screenWidth && screenWidth >= 640 ? (
-          <section>
-            <Sidebar />
-          </section>
-        ) : (
-          <AnimatePresence>
-            {isOpen && (
-              <Menu setIsOpen={setIsOpen} menuWidth={-wByN(2 / 3)!}>
-                <Sidebar />
-              </Menu>
-            )}
-          </AnimatePresence>
-        )}
-        <div className="sm:px-2 sm:pt-8">
-          <ChatsHeader setIsOpen={setIsOpen} />
-          <main className="flex min-h-[calc(100vh-4.5rem)] flex-col  bg-neutral-300 bg-opacity-10 sm:min-h-[calc(100vh-6.5rem)] sm:pb-16">
-            <div className="flex-1 ">Main section</div>
-            <div className="px-4 pt-2">
-              <form className="flex items-center gap-4" onSubmit={handleSubmit}>
-                <input
-                  className="container rounded-full bg-neutral-500/10 px-4 py-2 outline-none transition-all duration-200"
-                  placeholder={`Message channel name`}
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                />
-                <AnimatePresence>
-                  {message.length > 0 && (
-                    <motion.button
-                      className="rounded-full bg-lime-400 p-2 hover:bg-lime-500"
-                      type="submit"
-                      disabled={!(message.length > 0)}
-                      variants={appearIntoView}
-                      initial="initial"
-                      animate="animate"
-                      exit="exit"
-                    >
-                      <IoMdSend className="h-5 w-5 text-white" />
-                    </motion.button>
-                  )}
-                </AnimatePresence>
-              </form>
-            </div>
-          </main>
-        </div>
+    <div className="mx-auto max-w-7xl sm:grid sm:grid-cols-[1fr_2fr]">
+      {screenWidth && screenWidth >= 640 ? (
+        <section>
+          <Sidebar />
+        </section>
+      ) : (
+        <AnimatePresence>
+          {isOpen && (
+            <Menu setIsOpen={setIsOpen} menuWidth={-wByN(2 / 3)!}>
+              <Sidebar />
+            </Menu>
+          )}
+        </AnimatePresence>
+      )}
+      <div className="sm:px-2 sm:pt-8">
+        <ChatsHeader setIsOpen={setIsOpen} />
+        <main className="flex min-h-[calc(100vh-4.5rem)] flex-col bg-neutral-300 bg-opacity-10 sm:min-h-[calc(100vh-6.5rem)] sm:pb-16">
+          <div className="flex-1">Main section</div>
+          <div className="px-4 pt-2 pb-2 sm:pb-0">
+            <form className="flex items-center gap-4" onSubmit={handleSubmit}>
+              <input
+                className="w-full rounded-full bg-neutral-500/10 px-4 py-2 outline-none transition-all duration-200"
+                placeholder={`Message channel name`}
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                type="text"
+                onKeyDown={(e) => e.key === "Enter" && handleSubmit(e)}
+              />
+              <AnimatePresence>
+                {message.length > 0 && (
+                  <motion.button
+                    className="rounded-full bg-lime-400 p-2 hover:bg-lime-500"
+                    type="submit"
+                    disabled={!(message.length > 0)}
+                    variants={appearIntoView}
+                    initial="initial"
+                    animate="animate"
+                    exit="exit"
+                  >
+                    <IoMdSend className="h-5 w-5 text-white" />
+                  </motion.button>
+                )}
+              </AnimatePresence>
+            </form>
+          </div>
+        </main>
       </div>
-    </>
+    </div>
   );
 };
 export default chats;
