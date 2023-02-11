30d20151baa19be0493df497592e82e6ae1718d6
Manthan Kuber <manthankuber873@gmail.com>
2023-02-11 23:39:27 +0530

    feat: create chats context

---
+++ src/context/chats.context.tsx
@@ -0,0 +1,7 @@
+import { createContext, ReactNode } from "react";
+import { ChatSearch } from "../pages/chats";
+
+const ChatsContext = createContext<ChatSearch | null>(null);
+const { Provider: ChatsProvider } = ChatsContext;
+
+export { ChatsContext, ChatsProvider };
