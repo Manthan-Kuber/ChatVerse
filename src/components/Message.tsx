8b9b51866966adffb09720593145cd745196d0ad
Manthan Kuber <manthankuber873@gmail.com>
2023-02-20 12:55:18 +0530

    refactor: remove unnecessary code

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -18,8 +18,8 @@ const Message = ({ message, currentUserId }: MessageProps) => {
       <span
         className={`${
           isSender
-            ? "justify-self-end rounded-br-md bg-lime-400 dark:bg-lime-500"
-            : "float-left rounded-bl-md bg-gray-400 dark:bg-gray-500 "
+            ? " rounded-br-md bg-lime-400 dark:bg-lime-500"
+            : " rounded-bl-md bg-gray-400 dark:bg-gray-500 "
         } w-fit rounded-full p-4`}
       >
         {message.body}
