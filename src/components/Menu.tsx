bccab37dc2bc7f15057302ea513a83189dc8269b
Manthan Kuber <manthankuber873@gmail.com>
2023-03-12 22:21:48 +0530

    feat(menu): animate only once on inital mount

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 import { fadeInOut } from "../animations/animations";
 import { IoMdClose } from "react-icons/io";
 import ThemeChanger from "./ThemeChanger";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 const Menu = ({
   children,
@@ -12,25 +13,38 @@ const Menu = ({
   menuWidth: number;
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
+  //1st Arg is key,2nd Arg is value
+  const [shouldAnimate, setShouldAnimate] = useLocalStorage(
+    "shouldAnimate",
+    true
+  );
+  const fadeInOutProps = {
+    key: "sidebar",
+    variants: { fadeInOut },
+    initial: "initial",
+    animate: "animate",
+    exit: "exit",
+  };
+  const slideInProps = {
+    initial: { x: menuWidth },
+    animate: {
+      x: 0,
+      transition: {
+        duration: 0.2,
+        ease: "linear",
+      },
+    },
+  };
   return (
     <motion.div
-      key="sidebar"
-      variants={fadeInOut}
-      initial="initial"
-      animate="animate"
-      exit="exit"
+      {...(shouldAnimate ? fadeInOutProps : {})}
       className="fixed top-0 left-0 z-10 min-h-screen w-full bg-black/10 shadow-md backdrop-blur-sm dark:bg-white/10"
     >
       <motion.section
-        initial={{ x: menuWidth }}
-        animate={{
-          x: 0,
-          transition: {
-            duration: 0.2,
-            ease: "linear",
-          },
+        {...(shouldAnimate ? slideInProps : {})}
+        onAnimationComplete={() => {
+          setShouldAnimate(false);
         }}
-        exit={{ x: menuWidth }}
         className="min-h-screen w-8/12 bg-white py-4 dark:bg-[#1c1b22]"
       >
         <div className="flex items-center justify-between px-2">
