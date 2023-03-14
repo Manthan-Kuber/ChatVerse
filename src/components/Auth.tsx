ace6d58f75f6961dbcb679d5d6011bbc9edc4446
Manthan Kuber <manthankuber873@gmail.com>
2023-03-14 08:52:31 +0530

    refactor: extract loader into separate component

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,26 +1,18 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import ChatVerseText from "./ChatVerseText";
+import ChatVerseLoader from "./ChatVerseLoader";
 
 function Auth({ children }: { children: React.ReactNode }) {
   const { status } = useSession();
   const { replace } = useRouter();
 
   if (status === "loading") {
-    return (
-      <div className="grid min-h-screen w-full  place-items-center text-2xl">
-        <ChatVerseText className="animate-pulse" />
-      </div>
-    );
+    return <ChatVerseLoader />;
   }
 
   if (status === "unauthenticated") {
     replace("/auth/signin");
-    return (
-      <div className="grid min-h-screen w-full  place-items-center text-2xl">
-        <ChatVerseText className="animate-pulse" />
-      </div>
-    );
+    return <ChatVerseLoader />;
   }
 
   return <>{children}</>;
---
+++ src/components/ChatVerseLoader.tsx
@@ -0,0 +1,10 @@
+import ChatVerseText from "./ChatVerseText";
+
+const ChatVerseLoader = () => {
+  return (
+    <div className="grid min-h-screen w-full  place-items-center text-2xl">
+      <ChatVerseText className="animate-pulse" />
+    </div>
+  );
+};
+export default ChatVerseLoader;
