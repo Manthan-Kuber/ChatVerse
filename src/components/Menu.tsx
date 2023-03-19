69d50ba9e51a0e582db928b90b1becfd4868e7f4
Manthan Kuber <manthankuber873@gmail.com>
2023-03-19 10:01:58 +0530

    fix: add max height and overflow to fixed element

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -36,14 +36,14 @@ const Menu = ({
   return (
     <motion.div
       {...(shouldAnimate ? fadeInOutProps : {})}
-      className="fixed top-0 left-0 z-10 min-h-screen  w-full bg-black/10 shadow-md backdrop-blur-sm dark:bg-white/10"
+      className="z-1 fixed top-0 left-0 max-h-full w-full overflow-y-scroll bg-black/10 shadow-md backdrop-blur-sm dark:bg-white/10"
     >
       <motion.section
         {...(shouldAnimate ? slideInProps : {})}
         onAnimationComplete={() => {
           setShouldAnimate(false);
         }}
-        className="min-h-screen w-11/12 overflow-y-scroll  bg-white py-4 dark:bg-[#1c1b22]"
+        className="min-h-screen w-11/12 overflow-y-scroll bg-white py-4 dark:bg-black "
       >
         <div className="flex items-center justify-between px-2">
           <ThemeChanger />
--- src/pages/chats.tsx
+++ src/pages/chats.tsx
@@ -288,7 +288,7 @@ const chats = ({ chats, fetchError, currentUserId }: ChatProps) => {
 
   return (
     <motion.div
-      className="relative mx-auto max-w-7xl sm:grid sm:grid-cols-[1fr_minmax(0,2fr)]" //For grid 1fr -> minmax(auto,1fr) meaning min is auto i.e based on width of content and max is 1fr that's 1 fraction of space. minmax(0,1fr) -> overrides this making the max width 1fr and not making min width contetn based
+      className="mx-auto max-w-7xl sm:grid sm:grid-cols-[1fr_minmax(0,2fr)]" //For grid 1fr -> minmax(auto,1fr) meaning min is auto i.e based on width of content and max is 1fr that's 1 fraction of space. minmax(0,1fr) -> overrides this making the max width 1fr and not making min width, content based
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
