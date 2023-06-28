56369d0ae6f8451d761092ca8e338fd24fb32700
Manthan Kuber <manthankuber873@gmail.com>
2023-06-28 18:50:31 +0530

    feat: virtualize message list

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -3,6 +3,8 @@ import Skeleton from "react-loading-skeleton";
 import MessageComponent from "./Message";
 import { GetChats } from "../server/common/getChats";
 import { getChatName } from "../utils/functions";
+import { FixedSizeList as List } from "react-window";
+import AutoSizer from "react-virtualized-auto-sizer";
 
 const MessageListSkeleton = ({ count }: { count?: number }) => {
   return (
@@ -31,7 +33,7 @@ type MessageListProps = {
   messageList: MessageType[] | undefined;
   isLoading: boolean;
   currentUserId: string | null;
-  currentChat: GetChats[0] | undefined,
+  currentChat: GetChats[0] | undefined;
   messagesEndRef: React.MutableRefObject<HTMLDivElement | null>;
 };
 
@@ -40,27 +42,39 @@ const MessageList = ({
   isLoading,
   messagesEndRef,
   currentUserId,
-  currentChat
+  currentChat,
 }: MessageListProps) => {
-
-  const chatName = getChatName(currentChat,currentUserId)
+  const chatName = getChatName(currentChat, currentUserId);
 
   if (isLoading) return <MessageListSkeleton count={12} />;
 
   if (!messageList || messageList.length === 0) return <></>;
 
   return (
-    <>
-      {messageList.map((message) => (
-        <MessageComponent
-          key={message.id}
-          chatName={chatName}
-          message={message}
-          currentUserId={currentUserId!}
-        />
-      ))}
-      <div ref={messagesEndRef} />
-    </>
+    <AutoSizer>
+      {({ height, width }) => (
+        <List
+          className="List"
+          height={height}
+          itemCount={messageList.length}
+          itemSize={100} //Height of a single message
+          width={width}
+          outerElementType="ul"
+        >
+          {({ index, style }) => (
+            <li style={style}>
+              <MessageComponent
+                key={messageList[index]!.id}
+                chatName={chatName}
+                message={messageList[index]!}
+                currentUserId={currentUserId!}
+              />
+            </li>
+          )}
+        </List>
+      )}
+    </AutoSizer>
+    // TODO: Scroll to end of div
   );
 };
 export default MessageList;
