26588d1ec9ca99539c782d1e7017410484f57293
Manthan Kuber <manthankuber873@gmail.com>
2023-06-30 12:18:02 +0530

    fix: limit text input if there is /n overflow

--- src/components/ChatInputForm.tsx
+++ src/components/ChatInputForm.tsx
@@ -19,13 +19,20 @@ const ChatInputForm = (props: {
 
   const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     props.setValue(e.target.value);
-    if (
-      textAreaRef.current &&
-      textAreaRef.current.textLength <= textAreaHeight * 6
-    ) {
-      textAreaRef.current.style.height = "auto";
-      textAreaRef.current.style.height =
-        Math.max(textAreaRef.current.scrollHeight, textAreaHeight) + "px";
+    if (textAreaRef.current) {
+      const numberOfLines =
+        textAreaRef.current.value.split(/\r\n|\r|\n/).length;
+      console.log(textAreaRef.current.textLength);
+      //count number of lines to handle whitespace and textlength to handle overflow with number of characters
+      if (
+        numberOfLines < 7 &&
+        textAreaRef.current.textLength <= textAreaHeight * 6
+      ) {
+        console.log(numberOfLines < 7);
+        textAreaRef.current.style.height = "auto";
+        textAreaRef.current.style.height =
+          Math.max(textAreaRef.current.scrollHeight, textAreaHeight) + "px";
+      }
     }
   };
 
@@ -38,7 +45,7 @@ const ChatInputForm = (props: {
       }}
     >
       <textarea
-        className="no-scrollbar w-full resize-none rounded-md border-2 border-neutral-600 bg-neutral-500/10 px-4 py-2 pr-10 outline-none transition-transform duration-200 focus:border-lime-300"
+        className="w-full resize-none rounded-md border-2 border-neutral-600 bg-neutral-500/10 px-4 py-2 pr-10 outline-none transition-transform duration-200 focus:border-lime-300"
         placeholder={props.placeholder}
         value={props.value}
         onChange={(e) => {
@@ -46,6 +53,7 @@ const ChatInputForm = (props: {
         }}
         ref={textAreaRef}
         rows={1}
+        maxLength={4096}
       />
       <motion.button
         className=" rounded-md bg-lime-400 p-3.5 transition-colors duration-200 hover:cursor-pointer hover:bg-lime-500 disabled:cursor-not-allowed disabled:bg-lime-600"
